1ce9bb9df4630b7ca9a60834ba5d8591ab36da03
machariamwangi <you@example.com>
2019-07-29 18:20:40 -0400

    configuring and creating of product source

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
-<?php
 
+<?php
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
